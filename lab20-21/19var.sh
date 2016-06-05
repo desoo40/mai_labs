4961b7fc3a90b9623d0266257c131e067842af51
desoo40 <deso40@gmail.com>
2016-06-05 16:09:22 -0700

    kek

--- lab20-21/19var.sh
+++ lab20-21/19var.sh
@@ -3,19 +3,14 @@
 # all *.h, *.c files in current dir and subdir
 # lines counts
 
-ls -R ./ | awk '
-/:$/&&f{s=$0;f=0}
-/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}
-NF&&f{ print s"/"$0 }' > t.mp 
+find . -regex ".*\.\(h\|cpp\|c\)" > tmp
 
-cat t.mp | grep "\.h$\|\.cpp$\|\.c$" > t2.mp
-
-max=`wc -l < t2.mp`
+max=`wc -l < tmp`
 i=1
 sum=0
 
 while [[ $i -lt $((max+1)) ]]; do
-	file=`cat t2.mp | head -$i | tail -1`
+	file=`cat tmp | head -$i | tail -1`
 	lines=`wc -l < $file`
 	echo $file '==' $lines
 	sum=$(($sum+$lines))
@@ -24,4 +19,4 @@ done
 
 echo $sum
 
-rm -rf *.mp
+rm -rf tmp
---
+++ lab20-21/26.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+find ./test > tmp
+
+max=`wc -l < tmp`
+i=1
+sum=0
+
+while [[ $i -lt $((max+1)) ]]; do
+	file=`cat tmp | head -$i | tail -1`
+
+	b=`ls -l $file | head -1 | awk '{print substr($0,0,2)}'`
+
+	if [[ "$b" == "t" ]]; then
+		#echo google
+		i=$(($i+1))
+		continue
+	fi
+
+	a=`ls -l $file | awk ' {print $1} ' | grep x`
+
+	if [[ "$a" ]]; then
+		sum=$((sum+1))
+		echo $file EXECUTED
+	fi
+
+	i=$(($i+1))
+done
+
+echo $sum
+
+rm -rf tmp
