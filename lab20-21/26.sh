1429f992e707cd823bb1900c15b48e68e1b93b09
desoo40 <deso40@gmail.com>
2016-06-11 13:02:44 -0700

    bla bla

--- lab20-21/26.sh
+++ lab20-21/26.sh
@@ -5,23 +5,18 @@ find ./* > tmp
 max=`wc -l < tmp`
 i=1
 sum=0
+prev="May"
 
 while [[ $i -lt $((max+1)) ]]; do
 	file=`cat tmp | head -$i | tail -1`
 
-	b=`ls -l $file | head -1 | awk '{print substr($0,0,2)}'`
+	b=`ls -l $file | head -1 | awk '{print $6}'`
 
-	if [[ "$b" == "t" ]]; then
+	if [[ "$b" == "$prev" ]]; then
 		#echo google
+		echo $file
 		i=$(($i+1))
 		continue
-	fi
-
-	a=`ls -l $file | awk ' {print $1} ' | grep x`
-
-	if [[ "$a" ]]; then
-		sum=$((sum+1))
-		echo $file EXECUTED
 	fi
 
 	i=$(($i+1))
