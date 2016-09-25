dc9ac0476e45ca0185f555cf55203803a02d132b
Den <Den@DEN-PC>
2016-09-25 19:47:35 +0300

    last

--- 3sem/DA/lab1/lab1/main.cpp
+++ 3sem/DA/lab1/lab1/main.cpp
@@ -10,6 +10,12 @@ public:
     Tloong value = 0;
 };
 
+class TParametrs {
+public:
+    Tloong capacity = MIN_CAP;
+    Tloong size_of_arr = 0;
+};
+
 int MaxRadix(Tloong size, TElement *arr) {
     Tloong max = 0;
 
@@ -38,40 +44,29 @@ int Digit(TElement elem, int i) {
             }
             tmp /= 10;
         }
-        else {            
-			if (i == 1) {
+        else if (i == 1) {
 				return (int)tmp;
-			}
-			else {
-				return 0;
-			}
         }
 		--i;
 	}
 }
 
-void RarixSort(TElement *arr, Tloong n, Tloong cap)
+void RarixSort(TElement *arr, TParametrs *par)
 {
-    if (!n) {
+    if (!par->size_of_arr) {
         return;
     }
-    //for i = 1 to m
-    int m = MaxRadix(n, arr);
+    int m = MaxRadix(par->size_of_arr, arr);
     int k = 10;
     int *C = new int[k];
-    TElement *B = new TElement[cap];
+    TElement *B = new TElement[par->capacity];
 
     for (int i = 1; i <= m; ++i) {
-        //    for j = 0 to k - 1
         for (int j = 0; j <= k - 1; ++j) {
-            //        C[j] = 0
             C[j] = 0;
         }
 
-        //        for j = 0 to n - 1
-        for (int j = 0; j <= n - 1; ++j) {
-            //            d = digit(A[j], i)
-            //            C[d]++
+        for (int j = 0; j <= par->size_of_arr - 1; ++j) {
             int d = Digit(arr[j], i);
             ++C[d];
         }
@@ -84,13 +79,13 @@ void RarixSort(TElement *arr, Tloong n, Tloong cap)
             count += tmp;
         }
       
-        for (int j = 0; j <= n - 1; ++j) {
+        for (int j = 0; j <= par->size_of_arr - 1; ++j) {
             int d = Digit(arr[j], i);
             B[C[d]] = arr[j];
             ++C[d];
         }
 
-		for (int j = 0; j <= n - 1; ++j) {
+		for (int j = 0; j <= par->size_of_arr - 1; ++j) {
 			arr[j] = B[j];
 		}
     }
@@ -99,32 +94,36 @@ void RarixSort(TElement *arr, Tloong n, Tloong cap)
     delete[]B;
 }
 
-int main(int argc, char const **argv) {
-    
-    TElement elem;
-    Tloong capacity = MIN_CAP;
-    Tloong size_of_arr = 0;
-    TElement *arr = new TElement[capacity];
+void ArrayFill(TElement *arr, TParametrs *par) {
+    TElement tmp;
 
-
-    for (Tloong i = 0; cin >> elem.key >> elem.value; ++i) {
-        if (size_of_arr == capacity) {
-            capacity *= 2;
-            arr = (TElement*)realloc(arr, sizeof(TElement) * capacity);
+    for (Tloong i = 0; cin >> tmp.key >> tmp.value; ++i) {
+        if (par->size_of_arr == par->capacity) {
+            par->capacity *= 2;
+            arr = (TElement*)realloc(arr, sizeof(TElement) * par->capacity);
         }
 
-        arr[i] = elem;
-        ++size_of_arr;
+        arr[i] = tmp;
+        ++par->size_of_arr;
     }
+}
 
-    cout << "RESULT: " << endl;
-
-	RarixSort(arr, size_of_arr, capacity);
-
-    for (Tloong i = 0; i < size_of_arr; ++i) {
-		cout << arr[i].key << " " << arr[i].value << endl;
+void RadixSortResultPrint(TElement* arr, TParametrs* par)
+{
+    for (Tloong i = 0; i < par->size_of_arr; ++i) {
+        cout << arr[i].key << " " << arr[i].value << endl;
     }
+}
 
+int main(int argc, char const **argv) {
+    TParametrs *par = new TParametrs;
+    TElement *arr = new TElement[par->capacity];
+
+    ArrayFill(arr, par);
+	RarixSort(arr, par);
+    RadixSortResultPrint(arr, par);
+
+    delete[]par;
     delete[]arr;
 
     return 0;
