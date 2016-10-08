8b2400d8c4a16c990e42ef700fd3b10184353d7b
deso40 <deso4@DESKTOP-VGPB71G>
2016-10-09 00:31:27 +0300

    all files from 2nd sem are moved to the same named pack

--- 3sem/OOP/lab2/lab2/tstack.cpp
+++ 3sem/OOP/lab2/lab2/tstack.cpp
@@ -1,39 +1,40 @@
 #include "TStack.h"
 TStack::TStack() : head(nullptr) {
-}
+}elete head;
+}
 TStack::TStack(const TStack& orig) {
- head = orig.head;
+    head = orig.head;
 }
 std::ostream& operator<<(std::ostream& os, const TStack& stack) {
- TStackItem *item = stack.head;
+    TStackItem *item = stack.head;
 
- while(item!=nullptr)
- {
- os << *item;
- item = item->GetNext();
- }
+    while (item != nullptr)
+    {
+        os << *item;
+        item = item->GetNext();
+    }
 
- return os;
+    return os;
 }
 void TStack::push(Triangle &&triangle) {
- TStackItem *other = new TStackItem(triangle);
- other->SetNext(head);
- head = other;
+    TStackItem *other = new TStackItem(triangle);
+    other->SetNext(head);
+    head = other;
 }
 bool TStack::empty() {
- return head == nullptr;
+    return head == nullptr;
 }
 Triangle TStack::pop() {
- Triangle result;
- if (head != nullptr) {
- TStackItem *old_head = head;
- head = head->GetNext();
- result = old_head->GetTriangle();
- old_head->SetNext(nullptr);
- delete old_head;
- }
- return result;
+    Triangle result;
+    if (head != nullptr) {
+        TStackItem *old_head = head;
+        head = head->GetNext();
+        result = old_head->GetTriangle();
+        old_head->SetNext(nullptr);
+        delete old_head;
+    }
+    return result;
 }
 TStack::~TStack() {
- delete head;
+    delete head;
 }
