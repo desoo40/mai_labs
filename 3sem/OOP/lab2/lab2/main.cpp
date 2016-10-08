2a438586345dd272496aabae8019f3aaaa56b135
deso40 <deso4@DESKTOP-VGPB71G>
2016-10-09 01:45:55 +0300

    ч

---
+++ 3sem/OOP/lab2/lab2/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include "triangle.h"
+#include "tstackItem.h"
+#include "tstack.h"
+// Simple stack on pointers
+int main(int argc, char** argv) {
+
+	TStack stack;
+
+	stack.push(Triangle(1, 1, 1));
+	stack.push(Triangle(2, 2, 2));
+	stack.push(Triangle(3, 3, 3));
+
+	std::cout << stack;
+
+
+	Triangle t;
+
+	t = stack.pop(); std::cout << t;
+	t = stack.pop(); std::cout << t;
+	t = stack.pop(); std::cout << t;
+
+	return 0;
+}
