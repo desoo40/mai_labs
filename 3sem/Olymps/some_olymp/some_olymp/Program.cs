4e8249d198ee0b978a3c69ebb0685247c7e9c2c5
deso40 <deso4@DESKTOP-VGPB71G>
2016-11-01 12:52:04 +0300

    ывсыв

---
+++ 3sem/Olymps/some_olymp/some_olymp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace some_olymp
+{
+    class Program
+    {
+        Stack<char> scopes = new Stack<char>();
+
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var MaxUnder = FindMaxUnder(line);
+
+        }
+
+        private static object FindMaxUnder(string line)
+        {
+            var ans = "";
+            int maxLenght = 0;
+            int currLenght = 0;
+
+            for (var i = 0; i < line.Length; ++i)
+            {
+                if (line[i] == ')' && currLenght == 0)
+                {
+                    
+                }
+            }
+
+            return ans;
+        }
+    }
+}
