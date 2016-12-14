0e5ce6b462c533be41755dd74b9df74bb70da0c0
Den <Den@DEN-PC>
2016-12-15 01:36:40 +0300

    parser finaly done

--- 3sem/LP/CP C-Sharp/CP C-Sharp/Program.cs
+++ 3sem/LP/CP C-Sharp/CP C-Sharp/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,6 @@ namespace CP_C_Sharp
     class Program
     {
         public static StreamReader Reader = File.OpenText("Sorokin Family.ged");
-        //public static StreamWriter Writer = File.CreateText("cp_sorokin.txt");
 
         static void Main(string[] args)
         { 
@@ -22,40 +22,57 @@ namespace CP_C_Sharp
             List<string> predicats = new List<string>();
 
             string tmp = "";
-            string ID = "";
-            string Name = "";
+            string id = "";
+            string name = "";
 
-            for (var i = 0; (tmp = Reader.ReadLine()) != null; i++)
+            while ((tmp = Reader.ReadLine()) != null)
             {
                 if (tmp.StartsWith("0 @I"))
-                {
-                    ID = tmp.Substring(3, 7);
-                }
+                    id = tmp.Substring(3, 7);
 
                 if (tmp.StartsWith("1 NAME"))
                 {
-                    Name = tmp.Substring(7);
-                    Name = Name.Replace("/", "");
+                    name = tmp.Substring(7);
+                    name = name.Replace("/", "");
 
-                    if (Name == " ")
-                    {
-                        Name = "Неизвестно";
-                    }
-                    dictOfMembers[ID] = Name;
-                    ID = "";
-                    
+                    if (name == " ")
+                        name = "Неизвестно";
 
+                    dictOfMembers[id] = name;
+                    id = "";
                 }
 
                 if (tmp.StartsWith("0 @F"))
-                {
                     predicats = PredicatsWriter(dictOfMembers, predicats);
-                }
             }
-
+            
 
             File.WriteAllLines("cp_sorokin.pl", predicats);
+            File.AppendAllText("cp_sorokin.pl", @"
+bro_or_sist(X, Y) :-
+    parents(X, F, M),
+    parents(Y, F, M),
+    X\=Y.
 
+double(X, Y) :-
+    parents(X, F1, M1),
+    parents(Y, F2, M2),
+    X\=Y,
+    
+    (bro_or_sist(F1, F2);
+    bro_or_sist(F1, M2);
+    bro_or_sist(M1, F2);
+    bro_or_sist(M1, M2)).
+    
+triple(X, Y) :-
+    parents(X, F1, M1),
+    parents(Y, F2, M2),
+    X\=Y,
+    
+   (double(F1, F2);
+    double(F1, M2);
+    double(M1, F2);
+    double(M1, M2)).");
         }
 
         private static List<string> PredicatsWriter(Dictionary<string, string> dict, List<string> predicats)
@@ -70,29 +87,33 @@ namespace CP_C_Sharp
             {
                 tmp = Reader.ReadLine();
 
-                if (tmp.StartsWith("1 HUSB"))
+                if (tmp == null)
                 {
-                    husband = tmp.Substring(8, 7);
+                    Console.WriteLine("Incorrect .ged file");
+                    return predicats;
                 }
+
+                if (tmp.StartsWith("1 HUSB"))
+                    husband = tmp.Substring(8, 7);
 
                 if (tmp.StartsWith("1 WIFE"))
-                {
                     wife = tmp.Substring(8, 7);
-                }
 
                 if (tmp.StartsWith("1 CHIL"))
-                {
                     children.Add(tmp.Substring(8, 7));
-                }
-
             }
 
 
-            foreach (var child in children)
-            {
-                string pred = string.Format("parents(\"{0}\", \"{1}\", \"{2}\").\n", dict[child], dict[husband], dict[wife].TrimEnd());
-                predicats.Add(pred);
-            }
+            //foreach (var child in children)
+            //{
+            //    string pred = string.Format("parents(\"{0}\", \"{1}\", \"{2}\").", dict[child], dict[husband].TrimStart(), dict[wife].TrimEnd());
+            //    predicats.Add(pred);
+            //}
+
+            // спасибо visual studio за упрощение кода
+
+            predicats.AddRange(children.Select(child =>
+                $"parents(\"{dict[child]}\", \"{dict[husband].TrimStart()}\", \"{dict[wife].TrimEnd()}\")."));
 
             return predicats;
         }
