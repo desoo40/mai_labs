f0ead14e77ab45012cad9979ab1d43bc1ad899c8
deso40 <deso4@DESKTOP-VGPB71G>
2016-12-21 00:44:42 +0300

    OPP to final

---
+++ 3sem/OOP/lab6/lab6/TAllocationBlock.h
@@ -0,0 +1,22 @@
+#ifndef TALLOCATIONBLOCK_H
+#define TALLOCATIONBLOCK_H
+#include <cstdlib>
+class TAllocationBlock {
+public:
+	 TAllocationBlock(size_t size,size_t count);
+	 void *allocate();
+	 void deallocate(void *pointer);
+	 bool has_free_blocks();
+
+	 virtual ~TAllocationBlock();
+	private:
+	 size_t _size;
+	 size_t _count;
+
+	 char *_used_blocks;
+	 void **_free_blocks;
+
+	 size_t _free_count;
+
+};
+#endif
