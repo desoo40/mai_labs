ff81919fdbdee14e1827f981e2e8c7b4256f8bb8
desoo40 <deso40@gmail.com>
2017-03-25 05:33:26 +0300

    lab4 OS done

--- 3sem/OS/lab4_OS/os_lab4/main.cpp
+++ 3sem/OS/lab4_OS/os_lab4/main.cpp
@@ -22,6 +22,9 @@ public:
     void Delete_File();
     void ShowDiapasone(size_t, size_t);
     void WriteToFile(size_t, string);
+    void DeleteFromFile(size_t, size_t);
+    unsigned long long ControlSum();
+    void FindPatter(string);
 };
 
 FileMapping::FileMapping() {
@@ -102,6 +105,7 @@ void FileMapping::Close_File() {
 }
 
 void FileMapping::Delete_File() {
+    Close_File();
     CloseHandle(hMapping);
     CloseHandle(hFile);
     isOpened = false;
@@ -118,7 +122,7 @@ void FileMapping::ShowDiapasone(size_t from, size_t to) {
         cerr << "File is not open." << endl;
         return;
     }
-    for (size_t i = from - 1; i < to; i++)
+    for (size_t i = from; i <= to; i++)
         cout << fPtr[i];
 
     cout << endl;
@@ -166,11 +170,16 @@ void FileMapping::WriteToFile(size_t position, string input) {
     CloseHandle(hFile);
     CloseHandle(tmpFile);
 
-    if (!DeleteFile(fName)) 
+    if (!DeleteFile(fName))
+    {
         cout << "Deleting failed. Error: " << GetLastError() << endl;
+        return;
+    }
     
-    if (!MoveFile(L"tmp", fName))
+    if (!MoveFile(L"tmp", fName)) {
         cout << "Renaming failed. Error: " << GetLastError() << endl;
+        return;
+    }
     
     hFile = CreateFile(fName,
         GENERIC_WRITE | GENERIC_READ | GENERIC_EXECUTE,
@@ -181,9 +190,9 @@ void FileMapping::WriteToFile(size_t position, string input) {
         NULL
     );
 
-    SetFilePointer(this->hFile, 0, NULL, FILE_BEGIN);
+    SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
 
-    hMapping = CreateFileMapping(this->hFile,
+    hMapping = CreateFileMapping(hFile,
         &security_attributes,
         PAGE_EXECUTE_READWRITE,
         0,
@@ -193,11 +202,11 @@ void FileMapping::WriteToFile(size_t position, string input) {
 
     if (hMapping == INVALID_HANDLE_VALUE) {
         cerr << "Can't create file mapping. Error: " << GetLastError() << endl;
-        CloseHandle(this->hFile);
+        CloseHandle(hFile);
         return;
     }
 
-    fPtr = (unsigned char *)MapViewOfFile(this->hMapping,
+    fPtr = (unsigned char *)MapViewOfFile(hMapping,
         FILE_MAP_ALL_ACCESS,
         0,
         0,
@@ -207,6 +216,152 @@ void FileMapping::WriteToFile(size_t position, string input) {
     cout << "Symbols written" << endl;
 }
 
+void FileMapping::DeleteFromFile(size_t position, size_t qty) {
+    if (!isOpened) {
+        cerr << "File is not open" << endl;
+        return;
+    }
+
+    if (position > fSize || position < 0) {
+        cerr << "Position is out of the file range" << endl;
+        return;
+    }
+
+    if (position + qty > fSize) {
+        cerr << "qty of deleted symbol out of file range" << endl;
+        return;
+    }
+
+    SECURITY_ATTRIBUTES security_attributes;
+    security_attributes.nLength = sizeof(security_attributes);
+    security_attributes.lpSecurityDescriptor = NULL;
+    security_attributes.bInheritHandle = TRUE;
+
+    HANDLE tmpFile = CreateFile(L"tmp",
+        GENERIC_WRITE | GENERIC_READ | GENERIC_EXECUTE,
+        FILE_SHARE_WRITE | FILE_SHARE_READ,
+        &security_attributes,
+        OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    DWORD WriteBytes;
+    SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
+
+    for (size_t i = 0; i < position; ++i)
+        WriteFile(tmpFile, &(fPtr[i]), sizeof(char), &WriteBytes, nullptr);
+
+    for (size_t j = position + qty; j < fSize; ++j)
+        WriteFile(tmpFile, &(fPtr[j]), sizeof(char), &WriteBytes, NULL);
+
+    UnmapViewOfFile(fPtr);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+    CloseHandle(tmpFile);
+
+    if (!DeleteFile(fName))
+    {
+        cout << "Deleting failed. Error: " << GetLastError() << endl;
+        return;
+    }
+
+    if (!MoveFile(L"tmp", fName)) {
+        cout << "Renaming failed. Error: " << GetLastError() << endl;
+        return;
+    }
+
+    hFile = CreateFile(fName,
+        GENERIC_WRITE | GENERIC_READ | GENERIC_EXECUTE,
+        FILE_SHARE_WRITE | FILE_SHARE_READ,
+        &security_attributes,
+        OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
+
+    hMapping = CreateFileMapping(hFile,
+        &security_attributes,
+        PAGE_EXECUTE_READWRITE,
+        0,
+        0,
+        L"map"
+    );
+
+    if (hMapping == INVALID_HANDLE_VALUE) {
+        cerr << "Can't create file mapping. Error: " << GetLastError() << endl;
+        CloseHandle(hFile);
+        return;
+    }
+
+    fPtr = (unsigned char *)MapViewOfFile(hMapping,
+        FILE_MAP_ALL_ACCESS,
+        0,
+        0,
+        fSize
+    );
+
+    cout << "Symbols deleted" << endl;
+}
+
+unsigned long long FileMapping::ControlSum() {
+    unsigned long long sum = 0;
+    for(size_t i = 0; i < fSize; ++i)
+        sum += (unsigned long long) fPtr[i];
+    
+    return sum;
+}
+
+void FileMapping::FindPatter(string pattern) {
+    if (!isOpened) {
+        cerr << "File is not open." << endl;
+        return;
+    }
+
+    if (pattern.size() > fSize) {
+        cerr << "Pattern can't be bigger then file" << endl;
+        return;
+    }
+
+    size_t lines = 1;
+    size_t sym = 1;
+
+    for (size_t i = 0; i < fSize; ++i, ++sym) {
+       if (fPtr[i] == '\n') {
+           ++lines;
+           sym = 1;
+       }
+
+        if (fPtr[i] == pattern[0]) {
+            bool found = true;
+            for(size_t j = 0; j < pattern.size(); ++j) {
+                if (!(fPtr[i + j] == pattern[j]))
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+                cout << "Pattern found in " << lines << " line " << sym << " symbol" << endl;
+        }
+    }
+}
+
+void hints() {
+    cout << "-----------------------------" << endl;
+    cout << "1 - Open file"                 << endl;
+    cout << "2 - Close file"                << endl;
+    cout << "3 - Delete file"               << endl;
+    cout << "4 - Display range in file"     << endl;
+    cout << "5 - Write symbols to file"     << endl;
+    cout << "6 - Delete symbols from file"  << endl;
+    cout << "7 - Control sum"               << endl;
+    cout << "8 - Find pattern"              << endl;
+    cout << "0 - End"                       << endl;
+    cout << "-----------------------------" << endl;
+}
 
 int main()
 {
@@ -218,25 +373,28 @@ int main()
 
     while(1) {
         int c = 0;
+        hints();
 
         cin >> c;
 
+
         if (c == 1) {
+            cout << "Enter name of file: ";
             WCHAR *name = (WCHAR*)malloc(sizeof(WCHAR*) * SIZE_OF_FILE_NAME);
             wcin >> name;
             map->Open_File(name);
         }
 
-        if (c == 2) {
+        if (c == 2) 
             map->Close_File();
-        }
+        
 
         if (c == 3)
             map->Delete_File();
 
         if (c == 4) {
             int a, b = 0;
-            cout << "Enter diapasone:" << endl;
+            cout << "Enter range:" << endl;
             cout << "From:" << endl;
             cin >> a;
             cout << "To:" << endl;
@@ -252,13 +410,40 @@ int main()
         }
 
         if (c == 5) {
-            int a = 0;
+
+            size_t a = 0;
             string s;
+            cout << "Enter position:" << endl;
 
             cin >> a;
+            cout << "Enter symbols to write:" << endl;
+
             cin >> s;
             map->WriteToFile(a, s);
         }
+
+        if (c == 6) {
+            int a, s = 0;
+            
+            cout << "Enter position:" << endl;
+            cin >> a;
+            cout << "Enter qty of symbols to Delete:" << endl;
+            cin >> s;
+            map->DeleteFromFile(a, s);
+        }
+
+        if (c == 7) {
+            cout << map->ControlSum() << endl;
+        }
+        if (c == 8) {
+            string s = "";
+            cout << "Enter pattern:" << endl;
+            cin >> s;
+
+            map->FindPatter(s);
+        }
+        cout << "-----------------------------" << endl;
+
     }
 
 }
