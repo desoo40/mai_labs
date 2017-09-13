c447c515b9c03158c8aa3ff65b254eb2f4fce608
desoo40 <deso40@gmail.com>
2017-09-14 02:35:35 +0300

    add firts lab CG

---
+++ 5sem/CG/lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawBox
+            // 
+            this.drawBox.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.drawBox.Location = new System.Drawing.Point(51, 109);
+            this.drawBox.Name = "drawBox";
+            this.drawBox.Size = new System.Drawing.Size(435, 255);
+            this.drawBox.TabIndex = 0;
+            this.drawBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 507);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.drawBox);
+            this.Name = "Form1";
+            this.Text = "lab1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 5sem/CG/lab1/lab1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen penblue = new Pen(Color.Blue, 1);
+            var pi = Math.PI;
+
+            var t = -pi/4;
+            var dt = pi/360;
+
+            t += dt;
+
+            var a = 100.0;
+            var r = (a * Math.Cos(2 * t)) / Math.Cos(t);
+
+            var x_ = (float)(drawBox.Width / 2.0);
+            var y_ = (float)(drawBox.Height / 2.0);
+
+            Graphics g = drawBox.CreateGraphics();
+
+            var x0 = r * Math.Sin(t);
+            var y0 = r * Math.Cos(t);
+
+            var x1 = r * Math.Sin(t);
+            var y1 = r * Math.Cos(t);
+
+            for (int i = 0; i < 180; i++)
+            {
+                t += dt;
+
+                g.DrawLine(penblue, x_ + (float)x0, y_ + (float)y0, x_ + (float)x1, y_ + (float)y1);
+
+                x0 = x1;
+                y0 = y1;
+
+                x1 = r * Math.Sin(t);
+                y1 = r * Math.Cos(t);
+
+                r = (a * Math.Cos(2 * t)) / Math.Cos(t);
+            }
+        }
+    }
+}
