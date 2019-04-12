51fea2aaa3f3b020734ac4db4a91391a1703b0dc
desoo40 <deso40@gmail.com>
2019-04-12 14:00:16 +0300

    дщд

--- 8sem/Econometrics/regr.py
+++ 8sem/Econometrics/regr.py
@@ -21,11 +21,11 @@ from sklearn.model_selection import train_test_split as train
 
 X_train, X_test, y_train, y_test = train(X, y, test_size=0.3, shuffle=False)
 
-print(X_train[0])
-print(y_train[0])
-print(X_test[0])
-print(y_test[0])
-print(len(y_test))
+# print(X_train[0])
+# print(y_train[0])
+# print(X_test[0])
+# print(y_test[0])
+# print(len(y_test))
 
 from sklearn.linear_model import LinearRegression
 
@@ -45,18 +45,23 @@ print('Coefficients: \n', reg.coef_)
 print('Variance score: ', reg.score(X, y))
 
 from sklearn import metrics
-
 print('MAE: ', metrics.mean_absolute_error(expected, predicted))
 print('MSE: ', metrics.mean_squared_error(expected, predicted))
 print('RMSE: ', np.sqrt(metrics.mean_squared_error(expected, predicted)))
 print('EVS: ', metrics.explained_variance_score(expected, predicted))
 print('R2: ', metrics.r2_score(expected, predicted))
 
+lol = []
+
+for i in range(0, len(expected)):
+    tmp = expected[i] - np.dot(X_test[i], reg.coef_[0]) - reg.intercept_
+    lol.append(tmp)
+
 err = expected - predicted
-dt = np.concatenate([expected, predicted, err], axis=1)
+dt = np.concatenate([expected, predicted, err, lol], axis=1)
 
 import pandas as pd
-df = pd.DataFrame(dt, columns = ['Expected', 'Predicted', 'Err'])
+df = pd.DataFrame(dt, columns = ['Expected', 'Predicted', 'Err', 'Residues'])
 df.to_csv('ep.csv', index=False)
 
 # import seaborn as sns; sns.set(color_codes=True)
